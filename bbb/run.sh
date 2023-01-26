b62f4d9eb2ac5fd59572defb577b4e57b21ae601
Nate G <50537531+CircuitCoder4696@users.noreply.github.com>
2023-01-26 20:11:57 +0000

    Was just testing something.  `./bbb/run.sh` is not working properly.

--- bbb/run.sh
+++ bbb/run.sh
@@ -3,8 +3,12 @@
 #runs input through ast twice. both outputs should match
 cat test_1 | python3 dump_tree.py | tee >(cat - >&2) | python3 dump_tree.py stats
 
+echo -----
+
 #test 2 is test 1 without structs, enums, and unions
 cat test_2 | python3 dump_tree.py | tee >(cat - >&2) | python3 dump_tree.py stats
+
+echo -----
 
 #test 3 is for forward types
 cat test_3 | python3 dump_tree.py | tee >(cat - >&2) | python3 dump_tree.py stats
